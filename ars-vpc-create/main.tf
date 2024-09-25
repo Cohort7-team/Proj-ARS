4c1cf3e1d5c5e3e4e404c0e9809b5c3c29161190
Cohort7-team <cohort7.hellocloud@gmail.com>
2024-09-25 15:25:56 +0100

    amend main.tf

--- ars-vpc-create/main.tf
+++ ars-vpc-create/main.tf
@@ -90,7 +90,7 @@ resource "aws_nat_gateway" "ars_ngw" {
 
 
 resource "aws_route" "private_route" {
-  route_table_id            = aws_route_table.ars_route.id
+  route_table_id            = aws_route_table.ars_private_route.id
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_nat_gateway.ars_ngw.id
 }
